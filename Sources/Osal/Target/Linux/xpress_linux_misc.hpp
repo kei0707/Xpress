27589dc251dcd8799e208820454a416b1e5de8c0
kei0707 <dot_es-335@live.jp>
2025-03-02 20:02:48 +0900

    chore: use clang-format

--- Sources/Osal/Target/Linux/xpress_linux_misc.hpp
+++ Sources/Osal/Target/Linux/xpress_linux_misc.hpp
@@ -1,32 +1,27 @@
 #pragma once
 
-#include <string>
-#include <mutex>
 #include <cstdint>
 #include <cstdio>
+#include <mutex>
+#include <string>
 
-
-namespace xpress::linux::misc::local
-{
-  static std::mutex gs_io_mutex_t;
+namespace xpress::linux::misc::local {
+static std::mutex gs_io_mutex_t;
 }
 
-namespace xpress::linux
-{
+namespace xpress::linux {
 
-  /**
-   * @brief 
-   *
-   * @tparam Args
-   * @param ap_fmt
-   * @param ...args
-   */
-  template<typename ... Args>
-  void printf(const char *ap_fmt,
-      Args const & ...args)
-    {
-      std::unique_lock<std::mutex> lock(xpress::linux::misc::local::gs_io_mutex_t);
-      ::printf(ap_fmt,args ...);
-      return;
-    }
+/**
+ * @brief
+ *
+ * @tparam Args
+ * @param ap_fmt
+ * @param ...args
+ */
+template <typename... Args>
+void printf(const char *ap_fmt, Args const &...args) {
+  std::unique_lock<std::mutex> lock(xpress::linux::misc::local::gs_io_mutex_t);
+  ::printf(ap_fmt, args...);
+  return;
 }
+} // namespace xpress::linux
